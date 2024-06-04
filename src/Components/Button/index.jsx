12c779711cc29d0a90f3dc0def59b1f76fc341c4
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-04 20:08:58 -0300

    Update index.jsx

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,10 +1,14 @@
 import { Component } from "react";
-import './styles.css';
+import "./styles.css";
 
 export class Button extends Component {
   render() {
     const { text, onClick, disabled } = this.props;
 
-    return <button disabled={disabled} className='button' onClick={onClick}>{text}</button>;
+    return (
+      <button disabled={disabled} className="button" onClick={onClick}>
+        {text}
+      </button>
+    );
   }
 }
