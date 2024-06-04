5658aa8a10a02ce160d2ee0b1547db08bdd71529
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-06-04 02:26:54 -0300

    Update index.jsx

--- src/Components/TextInput/index.jsx
+++ src/Components/TextInput/index.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function TextInput({ searchValue, handleChange }) {
   return (
     <>
-      <input className='input-de-pesquisa' onChange={handleChange} type="search" value={searchValue} />
+      <input placeholder="Pesquisar algum post" className='input-de-pesquisa' onChange={handleChange} type="search" value={searchValue} />
     </>
   );
 }
